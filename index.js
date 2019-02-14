4293a7efe932fecb3a41a0eeddced59ad878357b
eatmyvenom <jacobhagan2002@gmail.com>
2019-02-13 20:49:35 -0800

    hecko

---
+++ index.js
@@ -0,0 +1,49 @@
+const cp = require('child_process');
+const exec = cp.exec;
+const execs = cp.execSync;
+const spawn = cp.spawn;
+const spawns = cp.spawnSync;
+const fs = require('fs');
+const read = fs.readFile;
+const reads = fs.readFileSync;
+
+var originalPackage = JSON.parse(reads('package.json').toString());
+//console.log(originalPackage)
+var child;
+
+
+function autoRun(){
+    execs('git fetch');
+    var newPackage = JSON.parse(reads('package.json').toString());
+    console.log('scan...')
+
+    if(originalPackage.version < newPackage.version){
+        originalPackage = newPackage;
+        console.log('new version!!!\n')
+        restartProcess();
+    }
+}
+
+function startProcess(){
+    child = spawn(originalPackage.scripts.test,{shell:true});
+}
+
+function endProcess(){
+    child.kill();
+}
+
+function restartProcess(){
+    endProcess();
+
+    while(!child.killed){
+        // no code here just waiting
+    }
+
+    startProcess();
+}
+
+module.exports = () =>{
+    startProcess();
+
+    var autoTime = setInterval(autoRun, 10000); 
+}
